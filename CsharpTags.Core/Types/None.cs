6ec3d4ec7b7fce9b64d20f845a3a76fae5ec7ef9
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2025-11-27 00:27:32 -0400

    Add Conditional rendering
    Okay I forgot to commit this first, sorry

--- CsharpTags.Core/Types/None.cs
+++ CsharpTags.Core/Types/None.cs
@@ -1,7 +1,40 @@
+using System.Runtime.CompilerServices;
 using CsharpTags.Core.Interface;
 
 namespace CsharpTags.Core.Types
 {
+    /// <summary>
+    /// if-elseif-else syntax for HtmlElements
+    /// </summary>
+    public record HtmlConditionalRenderingBuilder : HtmlElement
+    {
+        /// <summary>
+        /// The current element of the conditional rendering builder.
+        /// </summary>
+        public required HtmlElement Element { get; init; }
+
+        /// <summary>
+        /// In case the previous condition failed, check this other one.
+        /// </summary>
+        public HtmlConditionalRenderingBuilder ElseIf(bool flag, HtmlElement element)
+            =>
+            Element == Prelude.None_ ? flag ?
+            new() { Element = element } :
+            this : this;
+
+        /// <summary>
+        /// Provide a default value in case the previous conditions failed.
+        /// </summary>
+        public HtmlElement Else(HtmlElement element)
+            => Element == Prelude.None_ ? element : Element;
+
+        /// <inheritdoc/>
+        public override string Render()
+        {
+            return Element.Render();
+        }
+    }
+
     public static partial class Prelude
     {
         /// <summary>
@@ -15,7 +48,23 @@ namespace CsharpTags.Core.Types
         /// <summary>
         /// Helper for rendering the element if <paramref name="flag" /> is true
         /// </summary>
-        public static HtmlElement RenderWhen(bool flag, HtmlElement element)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static HtmlElement WhenH(bool flag, HtmlElement element)
             => flag ? element : None_;
+
+        /// <summary>
+        /// Helper for rendering the element if <paramref name="flag" /> is false
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static HtmlElement UnlessH(bool flag, HtmlElement element)
+            => flag ? None_ : element;
+
+        /// <summary>
+        /// Like WhenH, but starts at conditional rendering builder.
+        /// Which means that you can render the element like in a normal
+        /// if-elseif-else statement
+        /// </summary>
+        public static HtmlConditionalRenderingBuilder IfH(bool flag, HtmlElement element)
+            => new() { Element = WhenH(flag, element) };
     }
 }
