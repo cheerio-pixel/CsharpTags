3d1b1e028b38088cf661f974d78c85e9ed9d825b
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2026-03-07 14:25:39 -0400

    Document component

--- CsharpTags.Core/Types/Component.cs
+++ CsharpTags.Core/Types/Component.cs
@@ -2,22 +2,53 @@ using CsharpTags.Core.Interface;
 
 namespace CsharpTags.Core.Types;
 
+/// <summary>
+/// Base class for creating reusable HTML components with a fluent, functional API.
+/// Components encapsulate HTML structure and logic, providing a type-safe way to 
+/// build complex UI elements that can be composed together.
+/// </summary>
 public abstract record Component
     : IHtml, IWrapHtmlElement
 {
+    /// <summary>
+    /// Builds the internal HTML structure of this component.
+    /// Override this method to define the component's rendered output.
+    /// </summary>
+    /// <returns>An IHtml element representing this component's content.</returns>
     protected abstract IHtml Build();
 
+    /// <summary>
+    /// Gets the HTML tag name used when this component is simplified.
+    /// This tag will wrap the result of <see cref="Build()"/>.
+    /// </summary>
     protected abstract string TagName { get; }
 
 
+    /// <summary>
+    /// Gets the sequence of HTML attributes associated with this component.
+    /// Attributes are lazily calculated from the component's content.
+    /// </summary>
     public Seq<HtmlAttribute> Attributes => Split.Item2;
+    
+    /// <summary>
+    /// Gets the sequence of child HTML elements associated with this component.
+    /// Children are lazily calculated from the component's content.
+    /// </summary>
     public Seq<HtmlElement> Children => Split.Item1;
 
     private (Seq<HtmlElement>, Seq<HtmlAttribute>)? _split;
 
 #if NET10_0_OR_GREATER
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Component"/> class with the specified content.
+    /// </summary>
+    /// <param name="content">The initial content (attributes and child elements) for this component.</param>
     public Component(params ReadOnlySpan<IHtml> content)
 #else
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Component"/> class with the specified content.
+    /// </summary>
+    /// <param name="content">The initial content (attributes and child elements) for this component.</param>
     public Component(params IHtml[] content)
 #endif
     {
